aa68c3d8dedf9d498bb8e0adc98c8c89b0f915bc
sinclaircooper <sinclaircooper@sinclaircooper-T7500.(none)>
2013-12-17 10:37:50 +0000

    commit3

--- filter_pre_edited.py
+++ filter_pre_edited.py
@@ -1,9 +1,12 @@
 #! /usr/bin/python 
 
 import sys 
-#import regex
 import re
 
+#The purpose pf this script is to check measure identity 
+
+
+#lists to store things in later 
 pipes = []
 dna_length = []
 headers = []
@@ -13,38 +16,40 @@ coordinates2 = []
 grna = []
 gene = []
 
+#pattern for identifying the alignemnt 
 pattern =  re.compile(r'\||:|\.')
 
+
+#read in the alignment output from EMBOSS water
 for line in sys.stdin:
-	if '#' in line: continue
+	if '#' in line: continue 				#ignore comments in the alignment 
 	if pattern.findall(line):
-		pipes.append(line.count(':')+line.count('|'))	
+		pipes.append(line.count(':')+line.count('|'))	#identify all matches and count them
 	if 'Tb' in line:
-		if '#' in line: continue 
+		if '#' in line: continue 			#store the portion of the ref seq that matches
 		l = line.split()
 		dna_length.append(len(l[2]))
 		coordinates1.append(l[1])
 		gene.append(l[0])
-	if 'SEQ' in line:
+	if 'SEQ' in line:					#store the ID and portion of the query seq that matches
 		if '#'  in line:continue
 		l = line.split()
 		headers.append(l[0])	
 		grna.append(l[2])
+
 count_matches = 0
 count_reads_that_do_not_match = 0
 
+#collate all the information you have just measured and stored
 for p, d,h,c1,g,gene in zip(pipes, dna_length,headers,coordinates1,grna,gene):
-	pipes_colons_percent = round(float(p)/float(d)*100, 0)
-	if pipes_colons_percent >= 90 and int(d) > 20:
+	pipes_colons_percent = round(float(p)/float(d)*100, 0)			#calculate the % identity
+	if pipes_colons_percent >= 90 and int(d) > 20:				#if the read matches the pre-edited db discard it
 		count_matches = count_matches +1 
-		print "#%s%s%s%s%s"%(h,d,c1,g,gene)		
-		#print "ident: "+str(pipes_colons_percent)	
+		print "#%s%s%s%s%s"%(h,d,c1,g,gene)			
 	else:
 		print "%s \tPC:%s %s %s %s %s"%(h,pipes_colons_percent,c1,d,gene,g)
 		count_reads_that_do_not_match = count_reads_that_do_not_match +1
-		#print "ident: "+str(pipes_colons_percent)
-#print "#reads that match target seqs = %s"%(count_matches)
-#print "#reads that do not match target seqs = %s"%(count_reads_that_do_not_match)
+
 
 
 
