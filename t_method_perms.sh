e5eac6c8b02dedb55e67534292a640a0359cdd4f
sinclaircooper <sinclaircooper@sinclaircooper-T7500.(none)>
2014-01-07 13:07:30 +0000

    commit_5

---
+++ t_method_perms.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+
+
+water -filter yes -bsequence /home/sinclaircooper/grna_extraction/edited_dna_1seq.fa -datafile /home/sinclaircooper/t_shell/t_matrix.txt -gapopen 100 -gapextend 10 -awidth3 200 -aformat pair | tr a-z A-Z | grep -E 'SCORE|READ|EDITED|BRUCEI|SEQ' |grep -v -E '# 1|# 2|/|RUNDATE|REPORT|ALIGNED' |tr -d "#" | awk '{print $1,$2,$3}'
+
